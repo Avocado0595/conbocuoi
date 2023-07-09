03f1a76831e74660e605f60f302cf358d0d85162
Tran Thanh Xuan <ttxuan0595@gmail.com>
2023-07-09 08:08:15 +0700

    [hotfix] fix information content

--- src/commands/information.ts
+++ src/commands/information.ts
@@ -8,15 +8,11 @@ export const informationEmbed = async (client: Client) => {
     return new EmbedBuilder()
         .setColor([0, 153, 255])
         .setTitle('Bot Con Bò Cười')
-
-        .setAuthor({ name: 'Bot Con Bò Cười', iconURL: config.avatarLink })
+        .setAuthor({ name: 'Bot Con Bò Cười', iconURL: avatarLink })
         .setDescription(
-            'Chào mừng đến với Bot Con Bò Cười!\nXem thông tin về bò dưới đây nhé!'
+            `Chào mừng đến với Bot Con Bò Cười!\nXem thông tin về bò dưới đây nhé!\n- Mỗi lần vắt sữa sẽ được **${config.minMilk} - ${config.maxMilk} lít sữa**\n- Khoảng cách giữa mỗi lần vắt sữa **${config.coolDownMilk / 60}phút**\n- Khoảng cách giữa mỗi lần cho ăn **${config.coolDownFeeding}giây**\n- Mỗi phút bò sẽ giảm **${config.decStrengthVal}%** sức mạnh\n- Mỗi lần cho ăn bò sẽ tăng **${config.incStrengthMin}%** - **${config.incStrengthMax}% sức mạnh**\n- Mỗi ngày bạn vắt được tối đa **${config.maxMilkPerDay} lít sữa**`
         )
         .setThumbnail(config.avatarLink)
-        .addFields(
-            { value: `- Khoảng cách giữa mỗi lần vắt sữa **${config.coolDownMilk / 60}phút**\n- Khoảng cách giữa mỗi lần cho ăn **${config.coolDownFeeding}giây**\n- Mỗi phút bò sẽ giảm **${config.decStrengthVal}%** sức mạnh\n- Mỗi lần cho ăn bò sẽ tăng **${config.incStrengthMin}%** - **${config.incStrengthMax}% sức mạnh**\n- Mỗi ngày bạn vắt được tối đa **${config.maxMilkPerDay} lít sữa**`, name: '\u200b' },
-        )
         .setTimestamp()
         .setFooter({
             text: `Bot được tạo ra bởi ${owners
