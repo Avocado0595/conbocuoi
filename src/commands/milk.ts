31dcb0ad6dff6b884a9c7e116f37565826fe20ab
nthduc <nguyenthaiduc0212@gmail.com>
2023-07-29 15:11:12 +0700

    milk slash

--- src/commands/milk.ts
+++ src/commands/milk.ts
@@ -6,65 +6,80 @@ import {
 	updateUser,
 	addUser,
 } from '../controllers/userController';
-import { Message } from 'discord.js';
-import { User } from '../models/userModel';
-import mongoose from 'mongoose';
+import { ChatInputCommandInteraction, Message } from 'discord.js';
+
 import  config  from '../config/config';
 import { randomRange, roundDouble } from '../helpers';
 
 
-const milk = async (message: Message) => {
-	const user = await getUser(message.author.id),
-		milk = roundDouble(randomRange(config.maxMilk, config.minMilk));
-	if (user) {
-		const newStrength = await decStrength(user),
-			diffTime = new Date(user.lastTimeTakeMilk).getTime() - new Date().getTime(),
-			diffSecond = Math.abs(Math.ceil(diffTime / 1000)),
-			timeLeft = config.coolDownMilk - diffSecond;
+const milk = async (message: Message | ChatInputCommandInteraction) => {
+    let userId: string;
+    let reply: (content: string) => void;
+	let tag: string;
 
-		if (timeLeft > 0) {
-			const timeLeftMin = Math.floor(timeLeft / 60),
-				timeLeftSec = timeLeft % 60;
-			message.reply(
-				`**${message.author.tag.split('#')[0]
-				}** vắt từ từ thôi nè ! Chờ ${timeLeftMin} phút ${timeLeftSec} giây nữa vắt tiếp nhé :face_with_spiral_eyes: !`
-			);
-		} else if (newStrength < 50)
-			message.reply(
-				'Bò đang đói :tired_face:, hãy cho bò ăn để có sữa nhé!\nDùng **b!anco** :ear_of_rice:  để cho bò ăn nè.'
-			);
-		else {
-			const totalMilkPerDay = await getTotalMilkByDay(user, new Date());
-			if (totalMilkPerDay <= config.maxMilkPerDay) {
-				message.reply(
-					`**${message.author.tag.split('#')[0]
-					}** vừa vắt được ${milk} lít sữa bò! :bucket:`
-				);
-				const editUser = {
-					totalMilk: roundDouble(user.totalMilk + milk),
-					numberOfCow: user.numberOfCow,
-					userId: message.author.id,
-					lastTimeTakeMilk: new Date(),
-					milkTank: [
-						...user.milkTank,
-						{ milk, takingTime: new Date() },
-					],
-				};
-				await updateUser(user._id, editUser);
-			} else {
-				message.reply(
-					`**${message.author.tag.split('#')[0]
-					}** bạn vừa vắt hết sữa hôm nay rồi :open_mouth: ! Hôm sau quay lại nhé :wink: !`
-				);
-			}
-		}
-	} else {
-		await addUser(message.author.tag, message.author.id, milk);
-		message.reply(
-			`Lần đầu tiên, **${message.author.tag.split('#')[0]
-			}** vừa vắt được ${milk} lít sữa bò!`
-		);
-	}
+    if (message instanceof Message) {
+        userId = message.author.id;
+		reply = (content: string) => message.reply(content);
+		tag = message.author.tag;
+    } else if (message instanceof ChatInputCommandInteraction) {
+        userId = message.user.id;
+        reply = (content: string) => message.reply(content);
+		tag = message.user.tag;
+    } else {
+        return;
+    }
+
+    const user = await getUser(userId),
+        milk = roundDouble(randomRange(config.maxMilk, config.minMilk));
+    if (user) {
+        const newStrength = await decStrength(user),
+            diffTime = new Date(user.lastTimeTakeMilk).getTime() - new Date().getTime(),
+            diffSecond = Math.abs(Math.ceil(diffTime / 1000)),
+            timeLeft = config.coolDownMilk - diffSecond;
+
+        if (timeLeft > 0) {
+            const timeLeftMin = Math.floor(timeLeft / 60),
+                timeLeftSec = timeLeft % 60;
+            reply(
+                `**${tag.split('#')[0]
+                }** vắt từ từ thôi nè ! Chờ ${timeLeftMin} phút ${timeLeftSec} giây nữa vắt tiếp nhé :face_with_spiral_eyes: !`
+            );
+        } else if (newStrength < 50)
+            reply(
+                'Bò đang đói :tired_face:, hãy cho bò ăn để có sữa nhé!\nDùng **b!anco** :ear_of_rice:  để cho bò ăn nè.'
+            );
+        else {
+            const totalMilkPerDay = await getTotalMilkByDay(user, new Date());
+            if (totalMilkPerDay <= config.maxMilkPerDay) {
+                reply(
+                    `**${tag.split('#')[0]
+                    }** vừa vắt được ${milk} lít sữa bò! :bucket:`
+                );
+                const editUser = {
+                    totalMilk: roundDouble(user.totalMilk + milk),
+                    numberOfCow: user.numberOfCow,
+                    userId,
+                    lastTimeTakeMilk: new Date(),
+                    milkTank: [
+                        ...user.milkTank,
+                        { milk, takingTime: new Date() },
+                    ],
+                };
+                await updateUser(user._id, editUser);
+            } else {
+                reply(
+                    `**${tag.split('#')[0]
+                    }** bạn vừa vắt hết sữa hôm nay rồi :open_mouth: ! Hôm sau quay lại nhé :wink: !`
+                );
+            }
+        }
+    } else {
+        await addUser(tag, userId, milk);
+        reply(
+            `Lần đầu tiên, **${tag.split('#')[0]
+            }** vừa vắt được ${milk} lít sữa bò!`
+        );
+    }
 };
 
 export default milk;
--- src/slashcommand/milk.ts
+++ src/slashcommand/milk.ts
@@ -1,67 +1,13 @@
-import config from '../config/config';
-import { addUser, decStrength, getTotalMilkByDay, getUser, updateUser } from '../controllers/userController';
-import randomRange from '../helpers/randomRange';
-import roundDouble from '../helpers/roundDouble';
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { ChatInputCommandInteraction } from 'discord.js';
+import { milk } from "../commands";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { ChatInputCommandInteraction } from "discord.js";
 
 const data = new SlashCommandBuilder()
-    .setName('milk')
-    .setDescription('milk (vắt sữa bò)');
-    
+  .setName("milk")
+  .setDescription("milk (vắt sữa bò)");
 
 async function execute(interaction: ChatInputCommandInteraction) {
-    const user = await getUser(interaction.user.id),
-    milk = roundDouble(randomRange(config.maxMilk, config.minMilk));
-if (user) {
-    const newStrength = await decStrength(user),
-        diffTime = new Date(user.lastTimeTakeMilk).getTime() - new Date().getTime(),
-        diffSecond = Math.abs(Math.ceil(diffTime / 1000)),
-        timeLeft = config.coolDownMilk - diffSecond;
-
-    if (timeLeft > 0) {
-        const timeLeftMin = Math.floor(timeLeft / 60),
-            timeLeftSec = timeLeft % 60;
-        interaction.reply(
-            `**${interaction.user.tag.split('#')[0]
-            }** vắt từ từ thôi nè ! Chờ ${timeLeftMin} phút ${timeLeftSec} giây nữa vắt tiếp nhé :face_with_spiral_eyes: !`
-        );
-    } else if (newStrength < 50)
-        interaction.reply(
-            'Bò đang đói :tired_face:, hãy cho bò ăn để có sữa nhé!\nDùng **b!anco** :ear_of_rice:  để cho bò ăn nè.'
-        );
-    else {
-        const totalMilkPerDay = await getTotalMilkByDay(user, new Date());
-        if (totalMilkPerDay <= config.maxMilkPerDay) {
-            interaction.reply(
-                `**${interaction.user.tag.split('#')[0]
-                }** vừa vắt được ${milk} lít sữa bò! :bucket:`
-            );
-            const editUser = {
-                totalMilk: roundDouble(user.totalMilk + milk),
-                numberOfCow: user.numberOfCow,
-                userId: interaction.user.id,
-                lastTimeTakeMilk: new Date(),
-                milkTank: [
-                    ...user.milkTank,
-                    { milk, takingTime: new Date() },
-                ],
-            };
-            await updateUser(user._id, editUser);
-        } else {
-            interaction.reply(
-                `**${interaction.user.tag.split('#')[0]
-                }** bạn vừa vắt hết sữa hôm nay rồi :open_mouth: ! Hôm sau quay lại nhé :wink: !`
-            );
-        }
-    }
-} else {
-    await addUser(interaction.user.tag, interaction.user.id, milk);
-    interaction.reply(
-        `Lần đầu tiên, **${interaction.user.tag.split('#')[0]
-        }** vừa vắt được ${milk} lít sữa bò!`
-    );
+  await milk(interaction);
 }
-};
 
 export { data, execute };
